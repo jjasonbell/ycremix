5c6cf12c44e71fcaa7a75bdd5b261617d2974a40
jjasonbell <j.jason.bell@gmail.com>
2017-03-04 11:38:15 -0600

    Initial commit for ycremix site

---
+++ js/wrand.js
@@ -0,0 +1,8 @@
+function weightedRand2(spec) {
+  var i, sum=0, r=Math.random();
+  for (i in spec) {
+    sum += spec[i];
+    if (r <= sum) return i;
+  }
+}
+weightedRand2({0:0.8, 1:0.1, 2:0.1});
