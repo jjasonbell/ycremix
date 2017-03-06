3554173ca17f66641e91fc9eda970519a42629fa
jjasonbell <j.jason.bell@gmail.com>
2017-03-06 14:33:33 -0600

    Testing my understanding of javascript.

--- js/wrand.js
+++ js/wrand.js
@@ -1,8 +1,8 @@
-function weightedRand2(spec) {
+function weightedRand(spec) {
   var i, sum=0, r=Math.random();
   for (i in spec) {
     sum += spec[i];
     if (r <= sum) return i;
   }
 }
-weightedRand2({0:0.8, 1:0.1, 2:0.1});
+var weightedRand({0:0.8, 1:0.1, 2:0.1});
