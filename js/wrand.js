dd33b1dc7c227dbbeadf5af15aebb09a88d5d538
jjasonbell <j.jason.bell@gmail.com>
2017-03-06 14:49:30 -0600

    testing

--- js/wrand.js
+++ js/wrand.js
@@ -1,8 +1,10 @@
-function weightedRand(spec) {
+/*function weightedRand(spec) {
   var i, sum=0, r=Math.random();
   for (i in spec) {
     sum += spec[i];
     if (r <= sum) return i;
   }
 }
-var weightedRand({0:0.8, 1:0.1, 2:0.1});
+/*var weightedRand({0:0.8, 1:0.1, 2:0.1});
+*/
+console.log('test')
